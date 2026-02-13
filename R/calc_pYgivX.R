a55b68f5dda789845bfddde847e27df725824630
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-02-13 14:44:38 -0500

    Add analysis_link argument to calc_pYgivX

--- R/calc_pYgivX.R
+++ R/calc_pYgivX.R
@@ -1,9 +1,9 @@
-calc_pYgivX = function(data, successes, failures = NULL, theta) {
+calc_pYgivX = function(data, successes, failures = NULL, theta, analysis_link) {
   mu_theta = as.numeric(data %*% theta) ## beta0 + beta1x + ... 
-  if (link == "logit") {
+  if (analysis_link == "logit") {
     pi_theta = 1 / (1 + exp(- mu_theta)) ## probability of success on a single trial
     trials = rep(1, nrow(data)) ## number of trials (1 = Bernoulli)
-  } else if (link == "log") {
+  } else if (analysis_link == "log") {
     pi_theta = exp(mu_theta) ## probability of success on a single trial
     trials = successes + failures ## number of trials 
   }
