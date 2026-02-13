bf5592a01fc7b5dccbc8802bad9772d2d565fe68
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-02-13 14:41:26 -0500

    Change arguments to successes and failures

--- R/calc_pYgivX.R
+++ R/calc_pYgivX.R
@@ -1,13 +1,13 @@
-calc_pYgivX = function(data, success_col, fail_col, theta) {
+calc_pYgivX = function(data, successes, failures, theta) {
   mu_theta = as.numeric(data %*% theta) ## beta0 + beta1x + ... 
   if (link == "logit") {
     pi_theta = 1 / (1 + exp(- mu_theta)) ## probability of success on a single trial
     trials = rep(1, nrow(data)) ## number of trials (1 = Bernoulli)
   } else if (link == "log") {
     pi_theta = exp(mu_theta) ## probability of success on a single trial
-    trials = data[, success_col] + data[, fail_col] ## number of trials 
+    trials = successes + failures ## number of trials 
   }
-  pYgivX = dbinom(x = data[, success_col], 
+  pYgivX = dbinom(x = successes, 
                   size = trials, 
                   prob = pi_theta)
 }
