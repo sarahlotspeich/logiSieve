c423099469592f6eb16d4abf56904d462bbf56c8
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2026-02-20 16:22:28 -0500

    Generalize calc_pYgivX in prep for grouped logistic regression

--- R/calc_pYgivX.R
+++ R/calc_pYgivX.R
@@ -1,12 +1,18 @@
 calc_pYgivX = function(data, successes, failures = NULL, theta, analysis_link) {
   mu_theta = as.numeric(data %*% theta) ## beta0 + beta1x + ... 
-  if (analysis_link == "logit") {
-    pi_theta = 1 / (1 + exp(- mu_theta)) ## probability of success on a single trial
-    trials = rep(1, nrow(data)) ## number of trials (1 = Bernoulli)
-  } else if (analysis_link == "log") {
-    pi_theta = exp(mu_theta) ## probability of success on a single trial
-    trials = successes + failures ## number of trials 
+  ## Number of trials 
+  if (length(failures) == 0) {
+    trials = rep(1, nrow(data)) ## If Bernoulli, define as 1
+  } else {
+    trials = successes + failures ## Otherwise, sum successes + failures
   }
+  ## Calculate probability of success on a single trial
+  if (analysis_link == "logit") {
+    pi_theta = 1 / (1 + exp(- mu_theta)) 
+  } else if (analysis_link == "log") {
+    pi_theta = exp(mu_theta) 
+  }
+  ## Evaluate binomial PMF
   pYgivX = dbinom(x = successes, 
                   size = trials, 
                   prob = pi_theta)
